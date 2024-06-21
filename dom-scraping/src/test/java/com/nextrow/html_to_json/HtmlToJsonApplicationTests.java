4de4e49abe0565478274ca1326909d12eb535f2a
ruchith29 <167510345+ruchith29@users.noreply.github.com>
2024-06-21 18:02:54 +0530

    Initial commit

---
+++ dom-scraping/src/test/java/com/nextrow/html_to_json/HtmlToJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nextrow.html_to_json;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtmlToJsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
